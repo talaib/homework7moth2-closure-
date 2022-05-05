c32501db1ac39cf5a79b465258224f8c9be106c7
Saule Suiunalieva <talaibekovnasaule@gmail.com>
2022-05-05 17:26:47 +0600

    number One Number Two Number

--- homework7moth2(closure)/main.swift
+++ homework7moth2(closure)/main.swift
@@ -4,8 +4,27 @@
 //
 //  Created by saule on 5/5/22.
 //
-
 import Foundation
+//number One:
+//print("Введите слово")
+//var word = readLine()
+//var letters = { () -> Int in
+//    var words = word?.count
+//    return words!
+//}
+//print(letters())
 
-print("Hello, World!")
 
+//number Two:
+//print("Введите сумму конвертации")
+//var sum = readLine()!
+//var dollar = 82.10
+//var operation:  () -> Double = {return (Double(sum) ?? 0) * dollar}
+//print(operation())
+//
+//
+
+//number Three:
+//var names = ["Beka","Saule","Aika","Nuraiym","Ema"]
+//let sortedNames = names.sorted(by: {$0.count < $1.count})
+//print(sortedNames)
